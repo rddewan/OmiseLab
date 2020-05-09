c2e3972730336f6022c4be9327208df7965b5159
Richard <richarddewan@hotmail.com>
2020-05-09 23:02:29 +0700

    added required dependencies

---
+++ app/src/main/java/com/richarddewan/omiselab/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,16 @@
+package com.richarddewan.omiselab.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+
+}
