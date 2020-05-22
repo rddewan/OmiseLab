e1943039f7fcf4480fb7e4c4fb734822cf1b9e07
Richard <richarddewan@hotmail.com>
2020-05-22 12:09:06 +0700

    added util for network helper

--- app/src/main/java/com/richarddewan/omiselab/util/rx/RxSchedulerProvide.kt
+++ app/src/main/java/com/richarddewan/omiselab/util/rx/RxSchedulerProvide.kt
@@ -3,7 +3,9 @@ package com.richarddewan.omiselab.util.rx
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import javax.inject.Singleton
 
+@Singleton
 class RxSchedulerProvide: ScheduleProvider {
 
     override fun computation(): Scheduler = Schedulers.computation()
--- app/src/main/java/com/richarddewan/omiselab/util/rx/ScheduleProvider.kt
+++ app/src/main/java/com/richarddewan/omiselab/util/rx/ScheduleProvider.kt
@@ -1,7 +1,10 @@
 package com.richarddewan.omiselab.util.rx
 
 import io.reactivex.Scheduler
+import javax.inject.Singleton
 
+
+@Singleton
 interface ScheduleProvider {
 
     fun computation(): Scheduler
